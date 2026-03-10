109056b0509eb2bcaa57661ce4dffb4849f19df1
Alex Tabisz <a.tabisz@tricentis.com>
2026-03-10 12:50:38 +1100

    test(49-01): add failing tests for SevenSegmentEncoder
    - 12 DataRow cases covering digits 0-9, colon, space
    - 1 exception test for unsupported character
    - Tests fail to compile: SevenSegmentEncoder not yet implemented (RED)

---
+++ FuzzyClock.Core.Tests/SevenSegmentEncoderTests.cs
@@ -0,0 +1,31 @@
+using FuzzyClock.Core;
+
+namespace FuzzyClock.Core.Tests;
+
+[TestClass]
+public class SevenSegmentEncoderTests
+{
+    [TestMethod]
+    [DataRow('0', (byte)0x3F)]
+    [DataRow('1', (byte)0x06)]
+    [DataRow('2', (byte)0x5B)]
+    [DataRow('3', (byte)0x4F)]
+    [DataRow('4', (byte)0x66)]
+    [DataRow('5', (byte)0x6D)]
+    [DataRow('6', (byte)0x7D)]
+    [DataRow('7', (byte)0x07)]
+    [DataRow('8', (byte)0x7F)]
+    [DataRow('9', (byte)0x6F)]
+    [DataRow(':', (byte)0x80)]
+    [DataRow(' ', (byte)0x00)]
+    public void Encode_KnownCharacter_ReturnsExpectedMask(char c, byte expected)
+    {
+        Assert.AreEqual(expected, SevenSegmentEncoder.Encode(c));
+    }
+
+    [TestMethod]
+    public void Encode_UnsupportedCharacter_ThrowsArgumentException()
+    {
+        Assert.ThrowsException<ArgumentException>(() => SevenSegmentEncoder.Encode('X'));
+    }
+}
